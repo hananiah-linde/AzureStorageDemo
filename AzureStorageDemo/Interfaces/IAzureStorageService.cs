bff76357d5d9e6f95ca550ddfb651fe81a0e78ab
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2024-02-01 16:36:32 -0600

    add file support

--- AzureStorageDemo/Interfaces/IAzureStorageService.cs
+++ AzureStorageDemo/Interfaces/IAzureStorageService.cs
@@ -4,4 +4,6 @@ public interface IAzureStorageService
 {
     Task AddQueueMessageAsync<T>(string queueName, T data);
     Task AddQueueMessagesAsync<T>(string queueName, IEnumerable<T> data);
+    Task UploadFileAsync(IFormFile file, string containerName);
+    Task<byte[]> DownloadFileAsync(string containerName, string blobName);
 }
--- AzureStorageDemo/Services/AzureStorageService.cs
+++ AzureStorageDemo/Services/AzureStorageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using Azure.Storage.Blobs;
 using Azure.Storage.Queues;
 using AzureStorageDemo.Interfaces;
@@ -29,14 +30,31 @@ public class AzureStorageService(string connectionString) : IAzureStorageService
         await Task.WhenAll(taskList);
     }
 
-    public async Task UploadFileAsync()
+    public async Task UploadFileAsync(IFormFile file, string containerName)
     {
-        
+        var blobClient = GetBlobClient(containerName, file.FileName);
+
+        await using var stream = file.OpenReadStream();
+        await blobClient.UploadAsync(stream, true);
+    }
+    
+    public async Task<byte[]> DownloadFileAsync(string containerName, string blobName)
+    {
+        var blobClient = GetBlobClient(containerName, blobName);
+
+        var response = await blobClient.OpenReadAsync();
+        using var streamReader = new StreamReader(response);
+        var fileContent = Encoding.UTF8.GetBytes(await streamReader.ReadToEndAsync());
+        return fileContent;
     }
 
-    private BlobClient GetBlobClient()
+    private BlobClient GetBlobClient(string containerName, string blobName)
     {
-        return new BlobClient(new Uri(""));
+        var blobServiceClient = new BlobServiceClient(connectionString);
+        var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        var blobClient = containerClient.GetBlobClient(blobName);
+
+        return blobClient;
     }
 
     private async Task<QueueClient> GetQueueClient(string queueName)
